0cd33b60b4bc956724f70b2a392978311edbbb0d
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-01 18:19:36 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,5 +1,19 @@
-// Use this arrays for solution
-let array_1 = [3, 1, 3, 4, 2, 4, 12]
-let array_2 = [4, 15, 43, 1, 15, 1]
+const prompt = require('prompt');
+prompt.start();
 
-// Write code here. 
+
+prompt.get('input', function (err, result) {
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var array_n = input.split(','); // ['5', '2 4 7 12 9']
+    let array_3 = []
+    for(let i = 0; i<array_1.length;i++){
+       if(!array_2.includes(array_1[i])){
+        array_3.push(array_1[i])
+       }
+    }
+});
