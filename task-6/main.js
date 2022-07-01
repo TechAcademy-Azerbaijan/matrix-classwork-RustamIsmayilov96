704d4939df7a0bdddbc9a2526d7d75364ef1a8aa
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-01 19:40:31 +0300

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,15 +1,23 @@
-// Use this matrices for solution
+const prompt = require('prompt');
+prompt.start();
 
-let matrix_1 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]; 
 
-let matrix_2 = [
-    [1, 0, 1],
-    [0, 1, 0],
-    [1, 0, 1]
-]; 
+prompt.get('input', function (err, result) {
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var arr = input.split(','); // ['5', '2 4 7 12 9']
+    let sum = 0
 
-// Write code here. 
+    for(let i = 0; i<matrix_2.length;i++){
+        for(let j = 0; j<matrix_2[i].length;j++){
+         if(matrix_2[i][j]==1){
+            sum = sum + matrix_1[i][j]
+         }
+          
+        }
+    }console.log(sum);
+});
